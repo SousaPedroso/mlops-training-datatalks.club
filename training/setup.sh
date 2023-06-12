19ed991274eba143475efc21806de3419cb81bf5
SousaPedroso <gsousa844@gmail.com>
2023-06-12 19:08:07 -0400

    refactor: removed unused variable

--- training/setup.sh
+++ training/setup.sh
@@ -3,8 +3,6 @@
 # look for the dataset in cache first
 export HF_DATASETS_OFFLINE=1
 
-dataset_dir = $(pwd)/data
-
 conda create -n mlops-training python=3.10 pipenv
 conda activate mlops-training
 pipenv install .
