0971fd343c24b18ebed396831f5f7029f0d90704
SousaPedroso <gsousa844@gmail.com>
2023-08-20 12:38:30 -0400

    fix: prefect orchestration

--- training/setup.sh
+++ training/setup.sh
@@ -3,7 +3,8 @@
 # look for the dataset in cache first
 export HF_DATASETS_OFFLINE=1
 
-conda create -n mlops-training python=3.10 pipenv
+conda create -n mlops-training python=3.10.12 pipenv -y
 conda activate mlops-training
+pipenv install --dev --python=$(conda run which python) --site-packages
 pipenv install .
 pipenv shell
